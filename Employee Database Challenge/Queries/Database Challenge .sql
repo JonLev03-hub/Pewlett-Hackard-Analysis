d7f9dd5b108f362a83492c93358a632cf7d8d6c2
Jonathan Leverenz <jonlev03@gmail.com>
2021-11-09 19:54:11 -0500

    added one more block of code to the query list

--- Employee Database Challenge/Queries/Database Challenge .sql
+++ Employee Database Challenge/Queries/Database Challenge .sql
@@ -1,4 +1,3 @@
--- Create a list of employees ready to retire 
 SELECT e.emp_no,e.first_name,e.last_name,ti.title,ti.to_date,ti.from_date
 INTO retirement_titles
 FROM employees AS e
@@ -6,7 +5,13 @@ LEFT JOIN titles as ti
 ON e.emp_no = ti.emp_no
 WHERE (birth_date BETWEEN '1/1/1952' AND '12/31/1955')
 order by emp_no;
-select * from retirement_titles;
+select * from retirement_titles
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) first_name,last_name,title,to_date,from_date
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
 
 -- Use COUNT to retrieve the number of people per position
 SELECT title,COUNT(title)
@@ -14,9 +19,9 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
-SELECT retiring_titles;
 
 -- Create the list for the mentor program
+DROP TABLE temp_table;
 SELECT DISTINCT ON(d.emp_no,ti.emp_no)
 d.to_date,d.from_date,e.emp_no,e.first_name,e.last_name,e.birth_date,ti.title
 INTO temp_table
@@ -27,5 +32,6 @@ LEFT JOIN titles as ti
 ON e.emp_no = ti.emp_no
 WHERE (e.birth_date BETWEEN '1/1/1965' and '12/31/1965')
 AND (d.to_date = ('9999-01-01'));
-SELECT * FROM temp_table;
+SELECT * FROM temp_table
+
 
